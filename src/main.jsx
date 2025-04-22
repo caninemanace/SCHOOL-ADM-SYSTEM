3387f5a473f72e19cdcee3f86861af9753e5f386
Eddie Njoroge <eddie.njoroge2306@gmail.com>
2025-04-22 12:45:54 +0300

    routing problem is fiexed

--- src/main.jsx
+++ src/main.jsx
@@ -1,29 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import Applicants from './Applicants.jsx'
-import Form from './Form.jsx'
-import Instructions from './Instructions.jsx'
-import ErrorPage from './ErrorPage.jsx'
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Applicants from './Applicants.jsx';
+import Form from './Form.jsx';
+import Instructions from './Instructions.jsx';
+import ErrorPage from './ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element:<Applicants/>,
-    errorElement:<ErrorPage/>
+    path: '/',
+    element: <Applicants />,
+    errorElement: <ErrorPage />,
   },
   {
-    path:"/form",
-    element:<Form/>,
-    errorElement:<ErrorPage/>
+    path: '/form',
+    element: <Form />,
+    errorElement: <ErrorPage />,
   },
   {
-    path:"/Instructions",
-    element:<Instructions/>,
-    errorElement:<ErrorPage/>
-  }
-])
+    path: '/instructions',
+    element: <Instructions />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router}/>)
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
