4c29dec9e82b05238c0982765635b3d060ac0432
moreen karimi <moreenk541@gmail.com>
2025-04-23 10:58:56 +0300

    add and clear functinality

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,19 +1,70 @@
+import { useState } from "react";
 import NavBar from "./NavBar";
 
-function Form(){
+function Form({onAddApplicants}){
+    const [formData, setFormData] =useState({
+        name:"",
+        contact:"",
+        email:""
+       
+    });
+    function handleChange(e){
+        const{ name,value} = e.target ;
+        setFormData((prev)=> ({
+            ...prev,
+            [name] : value,
+            
+        }));
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        onAddApplicants(formData);
+        setFormData(
+            {
+                name:"",
+                contact:"",
+                email:""
+            }
+        )
+
+
+
+
+    }
+
+
+
+
     return(
         <>
             <NavBar/>
 
-            <form class="studForm" method=" POST"action="#" >
+            <form class="studForm" method=" POST"action="#" onSubmit={handleSubmit} >
                 <label>Name</label>
-                <input type="text" name="name" placeholder="Joe"></input>
+                <input 
+                type="text" 
+                name="name" 
+                placeholder="Joe"
+                value={formData.name}
+                onChange={handleChange}
+                />
 
                 <label>Contact</label>
-                <input type="number" name="contact" placeholder="07** *** ****"></input>
+                <input type="number"
+                 name="contact" 
+                 placeholder="07** *** ****"
+                 value={formData.contact}
+                 onChange={handleChange}
+
+                 />
 
                 <label>Email</label>
-                <input type="email" name="email" placeholder="joe@gmail.com"></input><br /><br /><br />
+                <input type="email" 
+                name="email" 
+                placeholder="joe@gmail.com"
+                value={formData.email}
+                onChange={handleChange}
+                /><br /><br /><br />
 
                 <button type="submit" className="bw-btn">SUBMIT</button>
 
